93c0f3cbd93ce67b4b296ad308ef3d0db848b9d8
Ivan <ivan.robak@gmail.com>
2022-06-21 20:39:15 +0300

    second

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        arrows: false,
+        slidesToShow: 2,
+        // arrows: false,
+        // dots: true
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay').fadeOut('slow')
+    });
+});
